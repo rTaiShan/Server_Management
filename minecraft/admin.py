a2e9ead829991f5f8ecdcaaff8aa3d865efb3643
rTaiShan <raul.tai@hotmail.com>
2023-07-17 23:57:07 -0300

    Registered minecraft models

--- minecraft/admin.py
+++ minecraft/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import MinecraftServer, ServerOwner
+from .models import MinecraftServer, MinecraftJar, ServerOwner, Player
 
-admin.site.register(ServerOwner)
 admin.site.register(MinecraftServer)
+admin.site.register(MinecraftJar)
+admin.site.register(ServerOwner)
+admin.site.register(Player)
